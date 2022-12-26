a605f5812a929b6d7d22ab05b176c9afd6c30d40
Godlikefreq <godlikefreq@gmail.com>
2022-12-26 20:05:03 +0300

    Users & Events views added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,18 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
-  # GET /users/1 or /users/1.json
   def show
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to user_url(@user), notice: "Информация о пользователе обновлена." }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
