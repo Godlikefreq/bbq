6c1f0d03f059f18337980716ee51c1d769e8ec52
Godlikefreq <godlikefreq@gmail.com>
2023-01-05 23:05:53 +0300

    Minor refactoring in subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,39 +3,38 @@ class Subscription < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :event, presence: true
-  validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
-  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
-  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
 
-  validate :self_subscribe, if: -> { user.present? }
-  validate :existing_email, unless: -> { user.present? }
+  with_options if: -> { user.present? } do
+    validate :self_subscription
+    validates :user, uniqueness: { scope: :event_id }
+  end
+
+  with_options unless: -> { user.present? } do
+    validate :email_presence
+    validates :user_name, presence: true
+    validates :user_email, uniqueness: { scope: :event_id }
+    validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+  end
 
   def user_name
-    if user.present?
-      user.name
-    else
-      super
-    end
+    user&.name || super
   end
 
   def user_email
-    if user.present?
-      user.email
-    else
-      super
-    end
+    user&.email || super
   end
 
-  def self_subscribe
+  private
+
+  def self_subscription
     if event.user == user
-      errors.add(:base, :self_subscribe)
+      errors.add(:user_email, :self_subscription)
     end
   end
 
-  def existing_email
+  def email_presence
     if User.find_by(email: user_email).present?
-      errors.add(:base, :existing_email)
+      errors.add(:user_email, :email_presence)
     end
   end
 end
