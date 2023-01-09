1c828f634284ab08b22bb2504209b2a18d503548
Godlikefreq <godlikefreq@gmail.com>
2023-01-09 22:12:55 +0300

    Fixed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  config.action_mailer.default_url_options = { :host => "mylessondomain.ru"}
+  config.action_mailer.default_url_options = { :host => "bbq.mylessondomain.ru"}
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
