ebe8e6b39e8e1e798cbb63f4acc7b3b9556047ee
Godlikefreq <godlikefreq@gmail.com>
2023-01-09 21:14:18 +0300

    Still fixing photo in emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.asset_host = 'http://bbq.mylessondomain.ru'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
