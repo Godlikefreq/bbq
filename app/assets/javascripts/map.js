3b40e45d574e080be0aebbb7f590df5965a946df
Godlikefreq <godlikefreq@gmail.com>
2023-01-14 17:06:03 +0300

    Fixed map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,10 @@
 document.addEventListener("turbo:load", function () {
     ymaps.ready(init);
-    var myMap;
 
     function init() {
         address = document.getElementById('map').getAttribute('data-address');
 
-        myMap = new ymaps.Map("map", {
+        var myMap = new ymaps.Map("map", {
             center: [55.76, 37.64],
             zoom: 10
         });
