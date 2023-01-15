2a7bcf315ca7fa2d2e0d0a9b88217e1668ee13ea
Godlikefreq <godlikefreq@gmail.com>
2023-01-15 21:16:53 +0300

    trying to add lightbox

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,5 @@
 import { Application } from "@hotwired/stimulus"
-import Lightbox from "stimulus-lightbox"
+import Lightbox from "bs5-lightbox"
 
 const application = Application.start()
 application.register('lightbox', Lightbox)
